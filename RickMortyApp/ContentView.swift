4b205f33641342428aa4a0a5e16160eb4ea15ae7
brayyanchristophertrujillovalle <brayyantrujil@gmail.com>
2024-06-20 18:12:06 -0500

    Initial Commit

---
+++ RickMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickMortyApp
+//
+//  Created by Brayyan Christopher Trujillo Valle on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hola Brayyan")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickMortyApp/RickMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppApp.swift
+//  RickMortyApp
+//
+//  Created by Brayyan Christopher Trujillo Valle on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
