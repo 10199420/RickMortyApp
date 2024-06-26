232057a60a9dabae7656009ba09c2d3df51e1642
brayyanchristophertrujillovalle <brayyantrujil@gmail.com>
2024-06-26 13:58:12 -0500

    Se realizo cambios.

--- RickMortyApp/ContentView.swift
+++ RickMortyApp/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        
             Text("Hola Brayyan")
+            VStack{
+                AsyncImage(url: viewModel.characterBasicInfo.image)
+                Text("Name:\(viewModel.characterBasicInfo.name)")
+                Text("First Episode:\(viewModel.characterBasicInfo.firstEpisodeTitle)")
+                Text("Dimension:\(viewModel.characterBasicInfo.dimension)")
+            }
+            .padding(.top, 32)
         }
-        .padding()
+        .onAppear{
+            Task{
+                await viewModel.executeRequest()
+            }
+            
+        }
     }
 }
 
--- RickMortyApp/ViewModel.swift
+++ RickMortyApp/ViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+
+final class ViewModel: ObservableObject {
+    @Published var characterBasicInfo: CharacterBasicInfo = .empty
+    
+    func executeRequest() async {
+        let characterURL = URL(string: "https://rickandmortyapi.com/api/character/1")!
+        
+        let (data, _) = try! await URLSession.shared.data(from: characterURL)
+        let characterModel = try! JSONDecoder().decode(CharacterModel.self, from: data)
+        print("Character Model \(characterModel)")
+        
+        let firstEpisodeURL = URL(string: characterModel.episode.first!)!
+        let (dataFirstEpisode, _) = try! await URLSession.shared.data (from: firstEpisodeURL)
+        let episodeModel = try! JSONDecoder().decode(EpisodeModel.self, from: dataFirstEpisode)
+        print("Episode Model \(episodeModel)")
+        
+        let characterLocationURL = URL(string: characterModel.locationURL)!
+        let (dataLocation, _) = try! await URLSession.shared.data (from: characterLocationURL)
+        let locationModel = try! JSONDecoder().decode(LocationModel.self, from: dataLocation)
+        print("Location Model \(locationModel)")
+        
+        DispatchQueue.main.async {
+            self.characterBasicInfo = .init(name: characterModel.name,
+                                            image: URL(string: characterModel.image)!,
+                                            firstEpisodeTitle: episodeModel.name,
+                                            dimension: locationModel.dimension)
+        }
+        
+       /* URLSession.shared.dataTask(with: characterURL) { data, response, error in
+            let characterModel = try! JSONDecoder().decode(CharacterModel.self, from: data!)
+            print("Character Model \(characterModel)")
+            
+            let firstEpisodeURL = URL(string: characterModel.episode.first!)!
+            
+            URLSession.shared.dataTask(with: firstEpisodeURL){ data, response, error in
+                let episodeModel = try! JSONDecoder().decode(EpisodeModel.self, from: data!)
+                print("Episode Model \(episodeModel)")
+                
+                let characterLocationURL = URL(string: characterModel.locationURL)!
+                
+                URLSession.shared.dataTask(with: characterLocationURL){ data, response, error in
+                    let locationModel = try! JSONDecoder().decode(LocationModel.self, from: data!)
+                    print("Location Model \(locationModel)")
+                    DispatchQueue.main.async {
+                        self.characterBasicInfo = .init(name: characterModel.name,
+                                                        image: URL(string: characterModel.image)!,
+                                                        firstEpisodeTitle: episodeModel.name,
+                                                        dimension: locationModel.dimension)
+                    }
+                }.resume()
+            }.resume()
+        }.resume()
+        */
+    }
+}
